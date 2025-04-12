2cfee7d66b388403cb04c2c94de7e06738ea0477
EC2 Default User <ec2-user@ip-172-31-38-173.ap-northeast-1.compute.internal>
2025-04-12 06:41:33 +0000

    画像投稿機能

--- app/models/list.rb
+++ app/models/list.rb
@@ -1,4 +1,5 @@
 class List < ApplicationRecord
-  #Listモデルに画像をつけたい
+  #Listモデルに画像をつけたい、has_one_attachedはRailsでファイル(画像やPDF)を一つ添付するための機能
+  #has_one_attached :ファイル名→erbも@user.ファイル名に変更しなければならない、fieldも:ファイル名に変更する必要がある
   has_one_attached :image
 end
